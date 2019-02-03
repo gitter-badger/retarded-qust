894374e84b36b5995c60009a322412af8b75f611
PragmaTwice <e.d.boy@aliyun.com>
2019-02-03 13:00:44 +0800

    Main: add argparse and un/pw input && list score by a beautiful style

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
 import point
 import login
+import argparse
 
-all_point = point.get_point(login.login('', ''))
-for single in (all_point['list']):
-    print('课程号:' + single['KCH'],
-          '成绩:' + str(single['KCCJ']),
-          '绩点:' + str(single['JD']),
-          '学分:' + str(single['XF']),
-          '学年:' + str(single['XN']),
-          '成绩填写日期:' + single['CJLRRQ'])
+parser = argparse.ArgumentParser(
+    description='Retarded QUST, aka 智障青科大, facilitate the destruction of the university for students')
+
+username_help = "input the username of i.qust.edu.cn, aka your student ID"
+password_help = "input the password of i.qust.edu.cn, the default one is the last six digits of your ID card"
+parser.add_argument('-u', '--username', help=username_help)
+parser.add_argument('-p', '--password', help=password_help)
+
+args = parser.parse_args()
+if not args.username:
+    args.username = input(username_help + "\n>> username: ")
+if not args.password:
+    args.password = input(password_help + "\n>> password: ")
+all_point = point.get_point(login.login(args.username, args.password))
+
+unformatted_header = "{:24s}|{:6s}|{:6s}|{:6s}|{:12s}|{:18s}"
+unformatted_body = "{:24s}|{:6.1f}|{:6.1f}|{:6.1f}|{:12s}|{:18s}"
+
+print(unformatted_header.
+      format('Course ID', 'Grade', 'Point', 'Credit', 'School year', 'Date of completion'))
+print(unformatted_header.
+      format("-" * 24, "-" * 6, "-" * 6, "-" * 6, "-" * 12, "-" * 18))
+for single in all_point['list']:
+    print(unformatted_body.
+          format(single['KCH'], single['KCCJ'], single['JD'], single['XF'], single['XN'], single['CJLRRQ']))
